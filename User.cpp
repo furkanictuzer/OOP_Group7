a9d4c7f4bdb10fe26b442cb774a86fb2d3a37d69
furkanictuzer <furkanictuzer@gmail.com>
2024-12-17 21:26:24 +0300

    User class added.

---
+++ User.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "User.h"
+#include <iostream>
+
+User::User(int id, const std::string& name, const std::string& pass, double bal)
+    : user_id(id), username(name), password(pass), balance(bal) {}
+
+std::string User::userInfo() const {
+    return "User ID: " + std::to_string(user_id) +
+           ", Username: " + username +
+           ", Balance: " + std::to_string(balance);
+}
+
+void User::addCategory(const Category& category) {
+    categories.push_back(category);
+}
+
+void User::removeCategory(const Category& category) {
+    for (auto it = categories.begin(); it != categories.end(); ++it) {
+        if (it->getCategoryName() == category.getCategoryName()) {
+            categories.erase(it);
+            break;
+        }
+    }
+}
+
+double User::getBalance() const {
+    return balance;
+}
+
+void User::updateUserInfo(const std::string& new_username, const std::string& new_password) {
+    username = new_username;
+    password = new_password;
+}
+
+bool User::deleteAccount() {
+    std::cout << "Account deleted.\n";
+    return true;
+}
+
+int User::getUserId() const {
+    return user_id;
+}
+
+vector<Category> User::getCategories() const
+{
+    return categories;
+}
---
+++ User.h
@@ -0,0 +1,39 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+#include <vector>
+
+#include "Category.h"
+#include "Budget.h"
+
+class User {
+private:
+    int user_id;
+    std::string username;
+    std::string password;
+    std::vector<Category> categories;
+    std::vector<Budget> budgets;
+    double balance;
+
+public:
+    User(int id, const std::string& name, const std::string& pass, double bal);
+
+    std::string userInfo() const;
+
+    void addCategory(const Category& category);
+
+    void removeCategory(const Category& category);
+
+    double getBalance() const;
+
+    void updateUserInfo(const std::string& new_username, const std::string& new_password);
+
+    bool deleteAccount();
+
+    int getUserId() const;
+
+    vector<Category> getCategories() const;
+};
+
+#endif // USER_H
