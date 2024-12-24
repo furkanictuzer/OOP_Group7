0d2595fd978dd1f51f143be5c5758da95a617666
Sumeyye F. Y. <147544943+SumeyyeFY@users.noreply.github.com>
2024-12-24 21:28:02 +0300

    NotificationManager class added

--- NotificationManager.cpp
+++ NotificationManager.cpp
@@ -1,21 +1,56 @@
 #include "NotificationManager.h"
 
-std::vector<Expense> NotificationManager::getExpensesToNotify() {
+NotificationManager::NotificationManager(MockClock mc)
+{
+    this->mockClock = mc;
+}
+
+std::vector<RepeatedExpense> NotificationManager::getExpensesToNotify() {
     return repeatedExpenses;
+}
+
+void NotificationManager::addExpense(RepeatedExpense newExpense)
+{
+    this->repeatedExpenses.push_back(newExpense);
 }
 
 std::vector<Budget> NotificationManager::getBudgetsToNotify() {
     return budgets;
 }
 
-void NotificationManager::checkBudgets() {
+void NotificationManager::addBudget(Budget newBudget)
+{
+    this->budgets.push_back(newBudget);
+}
+
+void NotificationManager::checkBudgets() 
+{
+
+    for(Budget budget: budgets)
+    {
+        if(budget.getSpentAmount() >= budget.getBudgetAmount() * 0.9)
+        {
+            string message = "Budget " + budget.getName() + " is near exceeding! There is " 
+                    + to_string(budget.getSpentAmount() - budget.getBudgetAmount()) + " left!\n";
+            notify(message);
+        }
+    }
     
 }
 
-void NotificationManager::checkExpenses() {
-    
+void NotificationManager::checkExpenses() 
+{
+    for(RepeatedExpense expense: repeatedExpenses)
+    {
+        if(expense.calculateNextOccurrence() <= mockClock.getCurrentTime() + std::chrono::hours(24))
+        {
+            string message = "The last pay date of " + expense.getExpenseDetails() + " is tomorrow. Do not forget to pay!\n";
+            notify(message);
+        }
+    }
 }
 
-void NotificationManager::notify() {
-    
+void NotificationManager::notify(string message) 
+{
+    cout << "New Notification from Expense Tracker App: \n" << message << endl;
 }
